e0252381236a2f1fb45c683283ab17e27713d3bb
NhiPhamnn <NhiPham.cv@gmail.com>
2021-09-26 19:48:40 +0700

    nhi

---
+++ SchoolOnl/SchoolOnl/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolOnl.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }  
+        public PartialViewResult KhoaDangHoc()
+        {
+            return PartialView();
+        }
+        public PartialViewResult KhoaChuaHoc()
+        {
+            return PartialView();
+        }
+        public PartialViewResult BaiVietHot()
+        {
+            return PartialView();
+        }
+        public PartialViewResult VideoHot()
+        {
+            return PartialView();
+        }
+        public ActionResult Hoc()
+        {
+            return View();
+        }
+        public ActionResult Hoi()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult HoiSingle()
+        {
+            return View();
+        }
+    }
+}
